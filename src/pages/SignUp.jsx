894a3d4ad914aeae2cd8966992e7cd454528ca70
osll2001 <octaviosuriellopezlopez@gmail.com>
2023-05-22 15:21:14 -0600

    Adición de formulario a page " signup"

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import FormSignUp from '../components/FormSignUp'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <>
+      <FormSignUp />
+    </>
   )
 }
 
