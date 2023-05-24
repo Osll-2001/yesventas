a8ec6d10d4b9ce5248957245df26ebf4db66efae
osll2001 <octaviosuriellopezlopez@gmail.com>
2023-05-24 11:55:31 -0600

    Implementacion component error404 v1

---
+++ src/components/ContainerError404.jsx
@@ -0,0 +1,11 @@
+
+const ContainerError404 = () => {
+  return (
+    <div className='containerError' style={{ textAlign: 'center' }}>
+      <h1 style={{ fontSize: '60px' }}>Error 404 :(</h1>
+      <h1>La Url No Existe</h1>
+    </div>
+  )
+}
+
+export default ContainerError404
