a3992f4b09434701980fb91479eb7b5dd9a90681
osll2001 <octaviosuriellopezlopez@gmail.com>
2023-06-07 12:37:06 -0600

    Implementacion footer basico

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footerMain'>
+      <p>Derechos de autor ©YesVentas 2023. Todos los derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import ContainerSale from "../components/ContainerSale"
-import NavHome from "../components/NavHome"
-
+import ContainerSale from '../components/ContainerSale'
+import Footer from '../components/Footer'
+import NavHome from '../components/NavHome'
 const Home = () => {
   return (
     <div>
       <NavHome />
+      {/* <ContainerSale /> */}
       <ContainerSale />
+      <Footer />
     </div>
   )
 }
