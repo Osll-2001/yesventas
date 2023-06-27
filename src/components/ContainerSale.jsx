c59df5d0c5ed53b6b90b1dfd1b21453193a43182
osll2001 <octaviosuriellopezlopez@gmail.com>
2023-06-26 21:37:15 -0600

    diseño info Cliente div

--- src/components/ContainerSale.jsx
+++ src/components/ContainerSale.jsx
@@ -4,7 +4,18 @@ const ContainerSale = () => {
   return (
     <div className='containerSale'>
       <section className='dataCostumer'>
-        <h1>Datos del cliente</h1>
+        <div className='searchCostumer'>
+          <label htmlFor='searchID'>ID CLIENTE:</label>
+          <input type='text' id='searchID' className='SearchID' placeholder='##' />
+        </div>
+        <div className='infoCostumer'>
+          <input type='text' className='nameCostumer' placeholder='Nombre del cliente' />
+          <input type='text' className='emailCostumer' placeholder='example@example.com' />
+        </div>
+        <div className='infoDocument'>
+          <input type='text' className='RFCCostumer' placeholder='RFC' />
+          <input type='text' className='folioSale' placeholder='## FOLIO' />
+        </div>
       </section>
       <section className='Sale'>
         <h1>Venta de los productos</h1>
